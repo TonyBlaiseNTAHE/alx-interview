cea69c862ad10c4afe74b08168ecb20bfe781caa
TonyBlaiseNTAHE <ntahetonyblaise@gmail.com>
2024-10-04 01:54:41 +0200

    adding readme file

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+A function that return a lists of integers representing the Pascal's triangle of n
+"""
+
+def pascal_triangle(n):
+    """Pascal triangle"""
+    ## a variable that represent the triangle
+    prototype = [[1]]
+    if n <= 0:
+        return []
+    else:
+        for i in range(1, n):
+            row = [1]
+            for j in range(1, i):
+                row.append(prototype[i-1][j-1]+ prototype[i-1][j])
+            row.append(1)
+            prototype.append(row)
+        return prototype
